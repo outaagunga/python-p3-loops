494af3d307e4062761253db7691a35e56fbabf0b
outaagunga <jeconiaagunga@gmail.com>
2023-08-19 15:08:10 +0300

    Completed the lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter-=1
+    print("Happy New Year!")
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
-def fizzbuzz():
-    # code goes here!
+    
     pass
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+pass
